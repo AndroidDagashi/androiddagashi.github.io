dfcdfee6a0e54e905f60ca6c7a0ba96b63d1e840
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2018-10-30 10:45:45 +0900

    :lipstick: reformat

--- components/ShareWidgets.vue
+++ components/ShareWidgets.vue
@@ -1,46 +1,46 @@
 <template>
-<div>
-  <no-ssr placeholder="loading share widgets...">
-    <div>
-      <span class="mr-2">
-        <a
-          href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-          class="twitter-share-button"
-          data-hashtags="AndroidDagashi"
-          data-show-count="false"
-        >Tweet</a>
-        <script
-          async
-          src="https://platform.twitter.com/widgets.js"
-          charset="utf-8"
-        />
-      </span>
-      <span>
-        <a
-          href="http://b.hatena.ne.jp/entry/"
-          class="hatena-bookmark-button"
-          data-hatena-bookmark-layout="basic-label-counter"
-          data-hatena-bookmark-lang="ja"
-          title="このエントリーをはてなブックマークに追加"
-        >
-          <img
-            src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
-            alt="このエントリーをはてなブックマークに追加"
-            width="20"
-            height="20"
-            style="border: none;"
+  <div>
+    <no-ssr placeholder="loading share widgets...">
+      <div>
+        <span class="mr-2">
+          <a
+            href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+            class="twitter-share-button"
+            data-hashtags="AndroidDagashi"
+            data-show-count="false"
+          >Tweet</a>
+          <script
+            async
+            src="https://platform.twitter.com/widgets.js"
+            charset="utf-8"
+          />
+        </span>
+        <span>
+          <a
+            href="http://b.hatena.ne.jp/entry/"
+            class="hatena-bookmark-button"
+            data-hatena-bookmark-layout="basic-label-counter"
+            data-hatena-bookmark-lang="ja"
+            title="このエントリーをはてなブックマークに追加"
           >
-        </a>
-        <script
-          type="text/javascript"
-          src="https://b.st-hatena.com/js/bookmark_button.js"
-          charset="utf-8"
-          async="async"
-        />
-      </span>
-    </div>
-  </no-ssr>
-</div>
+            <img
+              src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+              alt="このエントリーをはてなブックマークに追加"
+              width="20"
+              height="20"
+              style="border: none;"
+            >
+          </a>
+          <script
+            type="text/javascript"
+            src="https://b.st-hatena.com/js/bookmark_button.js"
+            charset="utf-8"
+            async="async"
+          />
+        </span>
+      </div>
+    </no-ssr>
+  </div>
 </template>
 <script lang="ts">
 import Component from 'nuxt-class-component';
