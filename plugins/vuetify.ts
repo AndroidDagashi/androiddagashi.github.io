6ea36534fad21d329f6b9fdf9985a850682a7844
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-07-29 01:18:57 +0900

    :construction:

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,18 +2,20 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import colors from 'vuetify/es5/util/colors'
 
+Vue.use(Vuetify)
+
 export default new Vuetify({
   theme: {
     dark: false,
     themes: {
       light: {
         primary: '#121212',
-    accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
-    info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3
       }
     }
   }
