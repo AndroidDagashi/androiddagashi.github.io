5649e1cb52aca65d852f7f161b28aa55445f70fe
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-11-13 14:36:22 +0900

    :lipstick: cleanup, schedule job

--- .github/push-resources-if-updated.sh
+++ .github/push-resources-if-updated.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 git remote add upstream https://${PUSH_ACCESS_TOKEN}@github.com/${PROJECT_USER_NAME}/${PROJECT_REPO_NAME}.git
 git add ./static/api ./static/feed.xml && git diff --cached --exit-code --quiet || git commit -m "Update stock info" && git push upstream development
-
-# if [[ -z "$(git status -s)" ]]; then
-#   echo "Nothing to commit"
-# else
-#   git add ./static/api
-#   git add ./static/feed.xml
-#   git commit --allow-empty -m "Update stock info"
-#   git remote add upstream https://${GITHUB_TOKEN}@github.com/${PROJECT_USERNAME}/${PROJECT_REPONAME}
-#   git push upstream development
-# fi
