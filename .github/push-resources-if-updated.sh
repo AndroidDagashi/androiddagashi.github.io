a34076a13683d4e58e3a5db6aae53bb2f74d604f
Yasuhiro Shimizu <the.phantom.bane@gmail.com>
2019-11-13 12:44:41 +0900

    :construction: push generated stuff

---
+++ .github/push-resources-if-updated.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git add ./static/api ./static/feed.xml && git diff --cached --exit-code --quiet || git commit -m "Update stock info" && git push origin development
+
+# if [[ -z "$(git status -s)" ]]; then
+#   echo "Nothing to commit"
+# else
+#   git add ./static/api
+#   git add ./static/feed.xml
+#   git commit --allow-empty -m "Update stock info"
+#   git remote add upstream https://${GITHUB_TOKEN}@github.com/${PROJECT_USERNAME}/${PROJECT_REPONAME}
+#   git push upstream development
+# fi
