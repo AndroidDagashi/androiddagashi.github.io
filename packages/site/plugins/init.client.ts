12e32de48c77eae04ee9130a0d8b41af6af512ca
yshrsmz <the.phantom.bane@gmail.com>
2020-06-28 17:08:46 +0900

    :memo:

--- packages/site/plugins/init.client.ts
+++ packages/site/plugins/init.client.ts
@@ -1,6 +1,10 @@
 import { Context } from '@nuxt/types'
 import { RootState } from '~/store'
 
+/**
+ * dispatch `nuxtServerInit` manually if state is not initialized
+ * https://github.com/nuxt/nuxt.js/issues/7051#issuecomment-604914461
+ */
 export default async function ({ app, store }: Context): Promise<void> {
   if (!(store.state as RootState).initialized) {
     await store.dispatch('nuxtServerInit', app.context)
