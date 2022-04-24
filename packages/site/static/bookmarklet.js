4e503d959b1c316aa8a23c6a8c52db7e60dc3aa8
yshrsmz <the.phantom.bane@gmail.com>
2022-04-24 17:35:20 +0900

    :fire: remove bookmarklet

--- packages/site/static/bookmarklet.js
+++
@@ -1,5 +0,0 @@
-export default (w) => {
-  const title = encodeURIComponent(w.document.title)
-  const url = encodeURIComponent(w.location.href)
-  w.location = `https://github.com/AndroidDagashi/AndroidDagashi/issues/new?title=${title}&body=${url}`
-}
