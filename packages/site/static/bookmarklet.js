7d3e5922c21ed0c38e9bf65c732526a9ed801c0b
yshrsmz <the.phantom.bane@gmail.com>
2022-04-24 17:20:09 +0900

    fix new issue url

--- packages/site/static/bookmarklet.js
+++ packages/site/static/bookmarklet.js
@@ -1,5 +1,5 @@
 export default (w) => {
   const title = encodeURIComponent(w.document.title)
   const url = encodeURIComponent(w.location.href)
-  w.location = `https://github.com/AndroidDagashi/AndroidDagashi/issue/new?title=${title}&body=${url}`
+  w.location = `https://github.com/AndroidDagashi/AndroidDagashi/issues/new?title=${title}&body=${url}`
 }
