72e3206090ae3aa2f12b1ed1f4d46460eb12c94f
yshrsmz <the.phantom.bane@gmail.com>
2023-10-22 17:20:01 +0900

    re-generate graphql-request files

--- packages/site-api-github/codegen.ts
+++ packages/site-api-github/codegen.ts
@@ -16,7 +16,11 @@ const config: CodegenConfig = {
         'typescript-graphql-request',
         {
           add: {
-            content: ['/* eslint-disable */'],
+            content: [
+              '/* eslint-disable */',
+              // https://github.com/dotansimha/graphql-code-generator-community/issues/458
+              `import type { GraphQLError } from 'graphql'`,
+            ],
           },
         },
       ],
